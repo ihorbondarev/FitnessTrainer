277671cf674edb306ef46a018d5af68caf18a2c9
Bondarev Igor <51752740+ihorbondarev@users.noreply.github.com>
2021-06-08 22:43:37 +0300

    Delete FitnessTrainer.MoblieApp directory

--- FitnessTrainer.MoblieApp/FitnessTrainer.MoblieApp/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-using Xamarin.Forms.Xaml;
-using FitnessTrainer.MoblieApp;
-using Android.App;
-
-[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
-[assembly: Application(UsesCleartextTraffic = true)]
